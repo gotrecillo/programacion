be34c5706be01c30bd88e52939191b85e759811e
gotrecillo <sergio.panadero.perez@gmail.com>
2014-12-06 01:15:24 +0100

    empiezo torres de hanoi

---
+++ TorresHanoi/Board.java
@@ -0,0 +1,15 @@
+
+public class Board {
+
+	private int[][] stick;
+	
+	public int[][] getPosition(){
+		return
+	}
+	
+	public Board(Disc disc){
+		int size = disc.getSize();
+		this.stick = new int[size][3];
+	}
+	
+}
