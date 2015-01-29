da8c19d60ca79b18f53dcd2e8d40d0c3d2a7b8e4
gotrecillo <sergio.panadero.perez@gmail.com>
2015-01-29 09:19:32 +0100

    semaforo

---
+++ POO/src/Semaforo.java
@@ -0,0 +1,76 @@
+/*
+ 3. Definir una clase que represente un semáforo: Semaforo. En la definición se debe incluir como atributos:
+El color del semáforo (VERDE,ROJO, AMBAR)
+Los métodos:
+Semáforo()
+Semafor(color)
+verSemaforo()
+toString()
+cambiarSemáforo()
+equals()
+ */
+
+public class Semaforo {
+
+	private ColSemaforo color;
+	
+	public Semaforo(){
+		color = ColSemaforo.NADA;
+	}
+	
+	public Semaforo(ColSemaforo color){
+		this.color = color;
+	}
+	
+	public void verSemaforo(){
+		switch (color){
+			
+			case NADA: 	System.out.println("( )");
+						System.out.println("( )");
+						System.out.println("( )");
+						break;
+			
+			case ROJO: 	System.out.println("(O)");
+						System.out.println("( )");
+						System.out.println("( )");
+						break;
+			
+			case AMBAR: System.out.println("( )");
+						System.out.println("(O)");
+						System.out.println("( )");
+						break;
+			
+			case VERDE: System.out.println("( )");
+						System.out.println("( )");
+						System.out.println("(O)");
+						break;
+			
+		}
+	}
+	
+	public String toString(){
+		return color.toString();
+	}
+	
+	public void cambiarSemaforo(){
+		switch (color){
+			
+			case ROJO: 	color = ColSemaforo.VERDE;
+						break;
+						
+			case AMBAR: 	color = ColSemaforo.ROJO;
+						break;
+			
+			case VERDE: color = ColSemaforo.AMBAR;
+						break;
+		}
+	}
+	
+	public boolean equals(Semaforo semaforo){
+		if (this.color == semaforo.color){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
